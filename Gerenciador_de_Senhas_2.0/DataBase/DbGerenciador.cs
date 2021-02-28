2fae42713d1e10cd70bb45c9ffd59a4f357cfc60
Stanley Felix Bergamo <stanley.felix.bergamo@outlook.com>
2021-02-28 16:49:47 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciador_de_Senhas_2.0/DataBase/DbGerenciador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Gerenciador_de_Senhas_2._0.DataBase
+{
+    class DbGerenciador : DbContext
+    {
+        public DbGerenciador() : base ("Gerencidor_De_Senhas_2.0"){}
+
+
+
+
+    }
+}
---
+++ Gerenciador_de_Senhas_2.0/View/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerenciador_de_Senhas_2._0
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
